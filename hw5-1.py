87ed544c977b5eaa54d43f3ab3901cd6bb56cf7f
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2021-10-22 09:58:54 -0400

    Add file

---
+++ hw5-1.py
@@ -0,0 +1,19 @@
+# Author JRI 10/22/21
+
+import random
+random.seed(16)
+
+# Question 1
+print(random.randint(31, 49))
+
+# Question 2
+print(random.randrange(4, 75, 2))
+
+# Question 3
+print(random.randrange(21, 31, 2))
+
+# Question 4
+print(random.randint(1, 8))
+
+# Question 5
+print(random.randint(1, 20))
